3ed81167714e7eddf57b8fe9e40fc94530f8c8b8
Ian Welerson <ian.nawes@gmail.com>
2021-12-05 18:03:28 -0300

    :white_check_mark: finish merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,38 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  const length = nums.length;
+  const middle = Math.round(length / 2);
+  const leftPart = nums.slice(0, middle);
+  const rightPart = nums.slice(middle);
+
+  const leftArray = mergeSort(leftPart);
+  const rightArray = mergeSort(rightPart);
+
+  return merge(leftArray, rightArray);
 };
+
+const merge = (left, right) => {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return results.concat(left, right);
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
