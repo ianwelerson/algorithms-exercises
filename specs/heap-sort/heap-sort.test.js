876ddcc163e3f912d1d27c235e8c885da65f429b
Ian Welerson <ian.nawes@gmail.com>
2021-12-12 12:54:49 -0300

    :white_check_mark: finish heap sort

--- specs/heap-sort/heap-sort.test.js
+++ specs/heap-sort/heap-sort.test.js
@@ -7,21 +7,54 @@
 */
 
 const heapSort = (array) => {
-  // code
+  createMaxHeap(array)
+  for (let index = array.length - 1; index > 0; index--) {
+    swapPlace(0, index, array)
+    heapify(array, 0, index)
+  }
+
   return array;
-};
+}
 
 const createMaxHeap = (array) => {
-  // code
-};
+  for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
+    heapify(array, i, array.length)
+  }
+
+  return array
+}
+
+const swapPlace = (index1, index2, array) => {
+  let temp = array[index1]
+  array[index1] = array[index2]
+  array[index2] = temp
+
+  return array
+}
 
 const heapify = (array, index, heapSize) => {
-  // code
-};
+  const left = 2 * index + 1
+  const right = 2 * index + 2
+
+  let maxIndex = index
+
+  if (heapSize > left && array[maxIndex] < array[left]) {
+    maxIndex = left
+  }
+
+  if (heapSize > right && array[maxIndex] < array[right]) {
+    maxIndex = right
+  }
+
+  if (maxIndex !== index) {
+    swapPlace(index, maxIndex, array)
+    heapify(array, maxIndex, heapSize)
+  }
+}
 
 // unit tests
 // do not modify the below code
-test.skip("heap sort", function () {
+test("heap sort", function () {
   const nums = [2, 5, 3, 8, 10, 6, 4, 7, 9, 1];
   heapSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
